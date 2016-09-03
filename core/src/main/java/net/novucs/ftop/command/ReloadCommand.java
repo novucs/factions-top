51872415e8b08a2e1ebf8d4a9bbdfd6a7028ef8f
novucs <contact@novucs.net>
2016-09-03 14:18:36 +0100

    Add a more insightful reload message

--- core/src/main/java/net/novucs/ftop/command/ReloadCommand.java
+++ core/src/main/java/net/novucs/ftop/command/ReloadCommand.java
@@ -1,5 +1,7 @@
 package net.novucs.ftop.command;
 
+import com.google.common.collect.ImmutableList;
+import mkremins.fanciful.FancyMessage;
 import net.novucs.ftop.FactionsTopPlugin;
 import net.novucs.ftop.PluginService;
 import org.bukkit.ChatColor;
@@ -9,6 +11,18 @@ import org.bukkit.command.CommandSender;
 
 public class ReloadCommand implements CommandExecutor, PluginService {
 
+    private static final FancyMessage MESSAGE = new FancyMessage("Configuration successfully reloaded. ")
+            .color(ChatColor.YELLOW)
+            .then("Hover for details.")
+            .color(ChatColor.LIGHT_PURPLE)
+            .tooltip(ImmutableList.of(
+                    ChatColor.YELLOW + "Without a full server resynchronization,",
+                    ChatColor.YELLOW + "new worth values will take a while",
+                    ChatColor.YELLOW + "to register. If you have modified any",
+                    ChatColor.YELLOW + "worth values and wish to see immediate",
+                    ChatColor.YELLOW + "changes, please type this:",
+                    ChatColor.LIGHT_PURPLE + "/ftoprec"
+            ));
     private final FactionsTopPlugin plugin;
 
     public ReloadCommand(FactionsTopPlugin plugin) {
@@ -33,8 +47,7 @@ public class ReloadCommand implements CommandExecutor, PluginService {
         }
 
         plugin.loadSettings();
-        sender.sendMessage(ChatColor.YELLOW + "FactionsTop settings have been successfully reloaded.");
-        sender.sendMessage(ChatColor.YELLOW + "New faction worth values will take a while to register.");
+        MESSAGE.send(sender);
         return true;
     }
 }
