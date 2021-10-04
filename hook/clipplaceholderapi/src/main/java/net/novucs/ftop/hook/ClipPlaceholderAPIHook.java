1a7314a59b4d95931544155308ada197dfe591c4
Phoenix616 <max@themoep.de>
2021-10-04 17:21:01 +0100

    Update PAPI hook to use PlaceholderExpansion
    Also fix that the enabledRanks config option wasnt applied

--- hook/clipplaceholderapi/src/main/java/net/novucs/ftop/hook/ClipPlaceholderAPIHook.java
+++ hook/clipplaceholderapi/src/main/java/net/novucs/ftop/hook/ClipPlaceholderAPIHook.java
@@ -1,19 +1,21 @@
 package net.novucs.ftop.hook;
 
-import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class ClipPlaceholderAPIHook implements PlaceholderHook {
+public class ClipPlaceholderAPIHook extends PlaceholderExpansion implements PlaceholderHook {
 
     private final Plugin plugin;
     private final Function<Player, String> playerReplacer;
     private final Function<Integer, String> rankReplacer;
     private final Supplier<String> lastReplacer;
+    private HashSet<Integer> enabledRanks;
 
     public ClipPlaceholderAPIHook(Plugin plugin,
                                   Function<Player, String> playerReplacer,
@@ -27,24 +29,49 @@ public class ClipPlaceholderAPIHook implements PlaceholderHook {
 
     @Override
     public boolean initialize(List<Integer> enabledRanks) {
-        return PlaceholderAPI.registerPlaceholderHook("factionstop", new me.clip.placeholderapi.PlaceholderHook() {
-            @Override
-            public String onPlaceholderRequest(Player player, String identifier) {
-                if ("name:last".equals(identifier)) {
-                    return lastReplacer.get();
-                } else if ("rank:player".equals(identifier)) {
-                    return playerReplacer.apply(player);
-                } else if (identifier.startsWith("name:")) {
-                    String[] split = identifier.split(":");
-                    if (split.length > 1) {
-                        try {
-                            int rank = Integer.parseInt(split[1]);
-                            return rankReplacer.apply(rank);
-                        } catch (NumberFormatException ignored) {}
+        this.enabledRanks = new HashSet<>(enabledRanks);
+        return register();
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, String identifier) {
+        if ("name:last".equals(identifier)) {
+            return lastReplacer.get();
+        } else if ("rank:player".equals(identifier)) {
+            return playerReplacer.apply(player);
+        } else if (identifier.startsWith("name:")) {
+            String[] split = identifier.split(":");
+            if (split.length > 1) {
+                try {
+                    int rank = Integer.parseInt(split[1]);
+                    if (enabledRanks.contains(rank) || enabledRanks.isEmpty()) {
+                        return rankReplacer.apply(rank);
                     }
+                } catch (NumberFormatException ignored) {
                 }
-                return null;
             }
-        });
+        }
+        return null;
+    }
+
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "factionstop";
+    }
+
+    @Override
+    public String getAuthor() {
+        return String.join(", ", plugin.getDescription().getAuthors());
+    }
+
+    @Override
+    public String getVersion() {
+        return plugin.getDescription().getVersion();
     }
 }
